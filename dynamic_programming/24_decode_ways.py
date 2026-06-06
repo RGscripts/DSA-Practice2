71fa825eba0e69a9b62f3e3d3a69702e18ea4421
Rajat Giri <rajatgiri192@gmail.com>
2026-06-06 11:31:18 +0530

    Add Decode Ways solution

---
+++ dynamic_programming/24_decode_ways.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        for i in range(2, n + 1):
+            one_digit = int(s[i - 1])
+            two_digit = int(s[i - 2:i])
+            if one_digit >= 1:
+                dp[i] += dp[i - 1]
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i - 2]
+        return dp[n]
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.numDecodings("12"))    
+    print(sol.numDecodings("226"))   
+    print(sol.numDecodings("06"))    
