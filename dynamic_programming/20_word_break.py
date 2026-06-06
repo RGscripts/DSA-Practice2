78297a960b2fe67894f9f0ff85cd7845a9548216
Rajat Giri <rajatgiri192@gmail.com>
2026-06-06 11:31:17 +0530

    Add Word Break solution

---
+++ dynamic_programming/20_word_break.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[n]
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.wordBreak("leetcode", ["leet", "code"]))         
+    print(sol.wordBreak("applepenapple", ["apple", "pen"]))      
+    print(sol.wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))  
