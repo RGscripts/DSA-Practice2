2cb7fe1f6777e308845eee7ed12786ab52632a0a
Rajat Giri <rajatgiri192@gmail.com>
2026-06-06 15:55:38 +0530

    Add Top K Frequent Elements solution

---
+++ heap/74_top_k_frequent_elements.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        freq = [[] for _ in range(len(nums) + 1)]
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        return res
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.topKFrequent([1,1,1,2,2,3], 2))
