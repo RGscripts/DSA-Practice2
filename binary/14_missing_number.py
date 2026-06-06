2434391d42efc38b2843674f2adaaa6203ae7d6f
Rajat Giri <rajatgiri192@gmail.com>
2026-06-06 11:31:14 +0530

    Add Missing Number solution

---
+++ binary/14_missing_number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.missingNumber([3, 0, 1]))       
+    print(sol.missingNumber([0, 1]))          
+    print(sol.missingNumber([9,6,4,2,3,5,7,0,1]))
