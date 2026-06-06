71e1827bd337583112054c6b511505320f2b173e
Rajat Giri <rajatgiri192@gmail.com>
2026-06-06 11:31:21 +0530

    Add Number of Islands solution

---
+++ graph/30_number_of_islands.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != '1':
+                return
+            grid[r][c] = '0'
+            dfs(r+1, c); dfs(r-1, c); dfs(r, c+1); dfs(r, c-1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    dfs(r, c)
+                    count += 1
+        return count
+
+if __name__ == "__main__":
+    sol = Solution()
+    grid1 = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+    print(sol.numIslands(grid1)) 
+
+    grid2 = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+    ]
+    print(sol.numIslands(grid2))  
